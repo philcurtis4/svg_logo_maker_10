15427d8dddb5190214abb1f0d3415f50f2f22f06
philcurtis422 <philcurtis422@gmail.com>
2024-07-17 21:41:17 -0400

    finished shape classes and got to render shape based on answers

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const inquirer = require('inquirer');
+
 
 const askUser = require('./lib/prompts');
 const createFile = require('./lib/createFile');
+const {Shapes, Circle, Triangle, Square, createShapeConstructor} = require('./lib/shapes');
+
+
 
 function init () {
-	askUser();
+	askUser()
+		.then((data) => {
+			const shape = createShapeConstructor(data)
+			shape.setColor(data.shapeColor);
+			shape.render();
+		})
 }
 
 init();
